594c9da7b398f1c36d651a20114f65dc381a0e38
Dima <Dima@Ilyas-MacBook-Air.local>
2020-03-14 14:55:00 +0200

    Updates

---
+++ precache-manifest.0558f25bddbcaacd1a2af8760e91b984.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619dbabd178506a3fcdc4beb259507d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4e7f511dad2df0d5d4b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0adae70abb625b319229",
+    "url": "/static/js/2.728239b7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.728239b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4e7f511dad2df0d5d4b",
+    "url": "/static/js/main.fe7c107c.chunk.js"
+  },
+  {
+    "revision": "c5b79674b16fc3dfdc80",
+    "url": "/static/js/runtime-main.c9848509.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
